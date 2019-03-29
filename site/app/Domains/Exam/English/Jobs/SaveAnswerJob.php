0b5a38764f92b847c4624bc043c3bee3e3b5e68b
shpp-dev <dev.kost.yakimenko@gmail.com>
2019-03-29 18:23:06 +0200

    fix save answer feature for english exam

--- site/app/Domains/Exam/English/Jobs/SaveAnswerJob.php
+++ site/app/Domains/Exam/English/Jobs/SaveAnswerJob.php
@@ -23,7 +23,6 @@ class SaveAnswerJob extends Job
 
     public function handle()
     {
-        $isCorrectAnswer = false;
         $task = json_decode(file_get_contents(base_path(ExamSystem::ENGLISH_QUESTIONS_PATH)), true)[$this->taskNumber];
         $results = json_decode($this->englishResult->results, true);
 
@@ -32,25 +31,23 @@ class SaveAnswerJob extends Job
         }
 
         if (!isset($results[$this->taskNumber])) {
-            $results[$this->taskNumber] = [
-                'question' => $task['question'],
-                'answers' => $task['answers'],
-                'answer' => $this->answer
-            ];
-
-            $this->englishResult->results = json_encode($results);
-            $this->englishResult->answersAmount++;
+            $isCorrectAnswer = false;
 
             if ($task['right'] == $this->answer) {
                 $this->englishResult->score++;
                 $isCorrectAnswer = true;
             }
 
-            $this->englishResult->save();
-        } else {
-            $isCorrectAnswer = $task['right'] == $results[$this->taskNumber][$this->answer];
-        }
+            $results[$this->taskNumber] = [
+                'question' => $task['question'],
+                'answers' => $task['answers'],
+                'answer' => $this->answer,
+                'correctAnswer' => $isCorrectAnswer
+            ];
 
-        return $isCorrectAnswer;
+            $this->englishResult->results = json_encode($results);
+            $this->englishResult->answersAmount++;
+            $this->englishResult->save();
+        }
     }
 }
