442815dcde7ca2b20ec4e552ec133ee2b77db1fe
shpp-dev <dev.kost.yakimenko@gmail.com>
2019-05-30 18:26:49 +0300

    chore: (programming, docker) return json for programming exam finish, change docker build for testing

--- site/app/Http/Controllers/ProgrammingController.php
+++ site/app/Http/Controllers/ProgrammingController.php
@@ -4,15 +4,23 @@
 namespace App\Http\Controllers;
 
 use App\Data\ExamSystem;
+use App\Domains\Http\Jobs\RespondWithJsonJob;
 use App\Features\Exam\Programming\GetProgrammingTaskFeature;
 use App\Features\Exam\Programming\SaveProgrammingAnswerFeature;
 use App\Features\Exam\Session\StartExamFeature;
 use App\Features\Exam\Session\FinishExamFeature;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Lucid\Foundation\Http\Controller as Controller;
+use Lucid\Foundation\JobDispatcherTrait;
+use Lucid\Foundation\MarshalTrait;
 
 
 class ProgrammingController extends Controller
 {
+    use MarshalTrait;
+    use DispatchesJobs;
+    use JobDispatcherTrait;
+
     public function start()
     {
         return $this->serve(StartExamFeature::class, ['examName' => ExamSystem::PROGRAMMING_EXAM_NAME]);
@@ -30,6 +38,12 @@ class ProgrammingController extends Controller
 
     public function finish()
     {
-        $this->serve(FinishExamFeature::class, ['examName' => ExamSystem::PROGRAMMING_EXAM_NAME]);
+        $finished = $this->serve(FinishExamFeature::class, ['examName' => ExamSystem::PROGRAMMING_EXAM_NAME]);
+
+        return $this->run(RespondWithJsonJob::class, [
+            'content' => [
+                'finished' => $finished
+            ]
+        ]);
     }
 }
