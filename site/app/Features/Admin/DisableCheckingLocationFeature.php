282f4edb469ec898fc9d08e144adadaa7755c1df
shpp-dev <dev.kost.yakimenko@gmail.com>
2019-10-21 15:26:09 +0300

    chore: (user) check user

--- site/app/Features/Admin/DisableCheckingLocationFeature.php
+++ site/app/Features/Admin/DisableCheckingLocationFeature.php
@@ -4,6 +4,8 @@
 namespace App\Features\Admin;
 
 
+use App\Data\ExamSystem;
+use App\Domains\Http\Jobs\RespondWithJsonErrorJob;
 use App\Domains\User\Jobs\DisableCheckingLocationForUserJob;
 use App\Domains\User\Jobs\GetUserByEmailJob;
 use Illuminate\Http\Request;
@@ -14,6 +16,14 @@ class DisableCheckingLocationFeature extends Feature
      public function handle(Request $request)
      {
          $user = $this->run(GetUserByEmailJob::class, ['email' => $request->email]);
+
+         if (!$user) {
+             return $this->run(RespondWithJsonErrorJob::class, [
+                 'code' => 404,
+                 'message' => ExamSystem::USER_NOT_FOUND
+             ]);
+         }
+
          $this->run(DisableCheckingLocationForUserJob::class, ['user' => $user]);
      }
 }
