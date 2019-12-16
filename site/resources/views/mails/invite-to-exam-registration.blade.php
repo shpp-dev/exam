b9241da20dc934e34f1f74085463837ea5621638
shpp-dev <dev.kost.yakimenko@gmail.com>
2019-12-16 15:04:12 +0200

    chore: add link to p2pZero to mail

--- site/resources/views/mails/invite-to-exam-registration.blade.php
+++ site/resources/views/mails/invite-to-exam-registration.blade.php
@@ -1,5 +1,6 @@
 <p>{{ __('email_messages.hello') }}!</p>
 <p>{{ __('email_messages.inviteToExam.p1') }}</p>
-<p>{{ __('email_messages.inviteToExam.p2') }}</p>
+<p>{{ __('email_messages.inviteToExam.p2'), ['p2pZeroUrl' => config('ptp.p2pZeroUrl')] }}</p>
 <p>{{ __('email_messages.inviteToExam.p3') }}</p>
-<p>{{ __('email_messages.inviteToExam.p4', ['calendlyUrl' => config('ptp.calendlyUrl')]) }}</p>
+<p>{{ __('email_messages.inviteToExam.p4') }}</p>
+<p>{{ __('email_messages.inviteToExam.p5', ['calendlyUrl' => config('ptp.calendlyUrl')]) }}</p>
