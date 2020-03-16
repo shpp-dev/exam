65f34faa4715922674b19516c6ca3e525f9e5772
shpp-dev <dev.kost.yakimenko@gmail.com>
2020-03-16 17:49:49 +0200

    chore: add link to account for user mail about exam result

--- site/resources/views/mails/exam-passed.blade.php
+++ site/resources/views/mails/exam-passed.blade.php
@@ -3,14 +3,13 @@
     @lang('email_subjects.examChecked')
 @endsection
 @section('subtitle')
-    @lang('email_messages.hello')!
+    @lang('email_messages.congratulation')!
 @endsection
 @section('content')
-    <p>@lang('email_messages.congratulation')!</p>
     <p>@lang('email_messages.examPassed.p1')</p>
     <p>@lang('email_messages.examPassed.p2')</p>
     <p>@lang('email_messages.examPassed.p3')</p>
     <p>@lang('email_messages.examPassed.p4')</p>
-    <p>@lang('email_messages.examPassed.p5')</p>
+    <p>@lang('email_messages.examPassed.p5', ['accountUrl' => config('ptp.accountFrontUrl')])</p>
     <p>@lang('email_messages.examPassed.p6')</p>
 @endsection
