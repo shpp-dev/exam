a6a22c4ad8a6fb6e5fe750670fbc18764ba11396
shpp-dev <dev.kost.yakimenko@gmail.com>
2020-12-16 21:59:55 +0200

    feat: send mails by supersender

--- site/app/Domains/Mail/Jobs/SendMailToUsersJob.php
+++ site/app/Domains/Mail/Jobs/SendMailToUsersJob.php
@@ -3,6 +3,7 @@
 namespace App\Domains\Mail\Jobs;
 
 
+use GuzzleHttp\Client;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 use Lucid\Foundation\Job;
@@ -46,8 +47,31 @@ class SendMailToUsersJob extends Job implements ShouldQueue
 
     public function handle()
     {
-        Mail::send($this->view, $this->data, function ($message) {
-            $message->to($this->emails)->subject($this->subject);
+        $this->emails[] = 'kost.yakimenko@gmail.com';
+
+        foreach ($this->emails as $email) {
+            if (config('supersender.enabled') === true && config('supersender.urls.' . $this->view)) {
+                $this->sendMailBySupersender($email);
+            } else {
+                $this->sendMailByMailer($email);
+            }
+        }
+    }
+
+    private function sendMailBySupersender(string $email)
+    {
+        $this->data['to'] = $email;
+
+        $client = new Client();
+        $client->post(config('supersender.urls.' . $this->view), [
+            'json' => $this->data
+        ]);
+    }
+
+    private function sendMailByMailer(string $email)
+    {
+        Mail::send($this->view, $this->data, function ($message) use ($email) {
+            $message->to($email)->subject($this->subject);
         });
     }
 }
